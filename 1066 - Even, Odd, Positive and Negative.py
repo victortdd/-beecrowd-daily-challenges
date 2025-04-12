6ecde343829f3fefeca0c74f7104f416dd69ab3f
victortdd <victorsantos.tdd@gmail.com>
2025-04-12 15:22:49 -0300

    feat: solved Beecrowd 1066

---
+++ 1066 - Even, Odd, Positive and Negative.py
@@ -0,0 +1,15 @@
+numbers = [int(x) for x in input().split()]
+negativo = 0
+positivo = 0
+par = 0
+impar = 0
+for number in numbers:
+    if number < 0:
+        negativo +=1
+    if number %2 == 0:
+        par +=1
+    if number %2 == 1:
+        impar +=1
+    if number > 0:
+        positivo +=1
+print (f"{par} valor(es) par(es)\n{impar} valor(es) impar(es)\n{positivo} valor(es) positivo(s)\n{negativo} valor(es) negativo(s)")
